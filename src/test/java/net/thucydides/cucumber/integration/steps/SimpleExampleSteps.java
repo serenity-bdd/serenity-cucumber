5ef9a56039c2020cafcdd13eea19257192efbdec
John Smart <john.smart@wakaleo.com>
2014-07-23 08:17:16 +1000

    Added a failing scenario for report generation

--- src/test/java/net/thucydides/cucumber/integration/steps/SimpleExampleSteps.java
+++ src/test/java/net/thucydides/cucumber/integration/steps/SimpleExampleSteps.java
@@ -11,12 +11,11 @@ public class SimpleExampleSteps {
 
     @Given("^I have a Cucumber feature file$")
     public void I_have_a_Cucumber_feature_file() throws Throwable {
-
     }
 
     @Then("^I should obtain a Thucydides report$")
     public void I_should_obtain_a_Thucydides_report() throws Throwable {
-        throw new PendingException();
+
     }
 
 }
