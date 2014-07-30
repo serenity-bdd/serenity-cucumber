fc8bdca9cdaf65992a97f45f309390c9c4bfba28
liviu.carausu <liviu.carausu@gmail.com>
2014-07-30 12:31:50 +0200

    test existence of test files

--- src/test/java/net/thucydides/cucumber/integration/steps/SimpleExampleSteps.java
+++ src/test/java/net/thucydides/cucumber/integration/steps/SimpleExampleSteps.java
@@ -1,10 +1,25 @@
 package net.thucydides.cucumber.integration.steps;
 
-import cucumber.api.PendingException;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import net.thucydides.core.guice.Injectors;
+import net.thucydides.core.webdriver.Configuration;
+
+
+
+import java.io.File;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasItem;
+
+import net.thucydides.core.util.FileSystemUtils;
+import org.apache.commons.io.FileUtils;
 
 public class SimpleExampleSteps {
+
 
     public SimpleExampleSteps() {
     }
@@ -16,7 +31,26 @@ public class SimpleExampleSteps {
 
     @Then("^I should obtain a Thucydides report$")
     public void I_should_obtain_a_Thucydides_report() throws Throwable {
-        throw new PendingException();
+        Configuration systemConfiguration = Injectors.getInjector().getInstance(net.thucydides.core.webdriver.Configuration.class);
+        File outputDirectory = systemConfiguration.getOutputDirectory();
+        File[] files = outputDirectory.listFiles();
+        int xmlCount = 0;
+        int jsonCount = 0;
+        int htmlCount = 0;
+        for(File f : files) {
+           if(f.getAbsolutePath().endsWith(".html")) {
+                htmlCount++;
+           } else if(f.getAbsolutePath().endsWith(".json")) {
+                jsonCount++;
+           }else if(f.getAbsolutePath().endsWith(".xml")) {
+                xmlCount++;
+           }
+        }
+        assert(xmlCount > 0);
+        assert(jsonCount > 0);
+        assert(htmlCount > 0);
+        assert(xmlCount == jsonCount);
+        assert(jsonCount == htmlCount);
     }
 
 }
