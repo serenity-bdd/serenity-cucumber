1df202ba1b99e05837ee7d5e8635a91ee5cf81d9
liviu.carausu <liviu.carausu@gmail.com>
2014-08-06 11:14:09 +0200

    web tests

---
+++ src/test/java/net/thucydides/cucumber/integration/SimpleSeleniumScenario.java
@@ -0,0 +1,10 @@
+package net.thucydides.cucumber.integration;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/samples/web/aPassingBehaviorWithSelenium.feature")
+public class SimpleSeleniumScenario {}
