7f85d822a7be755a33b1bb96f3db7305390dc710
liviu.carausu <liviu.carausu@gmail.com>
2014-08-23 15:16:26 +0200

    use CucumberWithThucydides for running tests

--- src/test/java/net/thucydides/cucumber/integration/DataDrivenScenario.java
+++ src/test/java/net/thucydides/cucumber/integration/DataDrivenScenario.java
@@ -2,11 +2,12 @@ package net.thucydides.cucumber.integration;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import net.thucydides.cucumber.CucumberWithThucydides;
 import org.junit.runner.RunWith;
 
 /**
  * Created by john on 23/07/2014.
  */
-@RunWith(Cucumber.class)
+@RunWith(CucumberWithThucydides.class)
 @CucumberOptions(features="src/test/resources/samples/web/dataDrivenBehavior.feature")
 public class DataDrivenScenario {}
--- src/test/java/net/thucydides/cucumber/integration/SimpleTableScenario.java
+++ src/test/java/net/thucydides/cucumber/integration/SimpleTableScenario.java
@@ -2,11 +2,12 @@ package net.thucydides.cucumber.integration;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import net.thucydides.cucumber.CucumberWithThucydides;
 import org.junit.runner.RunWith;
 
 /**
  * Created by john on 23/07/2014.
  */
-@RunWith(Cucumber.class)
+@RunWith(CucumberWithThucydides.class)
 @CucumberOptions(features="src/test/resources/samples/simple_table_based_scenario.feature")
 public class SimpleTableScenario {}
