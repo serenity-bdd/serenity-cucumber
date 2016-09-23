e687286160295177c41e995cd26d21e0fd47ba01
cliviu <liviu.carausu@gmail.com>
2016-09-23 23:02:00 +0200

    return false from hasHighPriority of CucumberTagProviderStrategy

--- src/main/java/net/serenitybdd/cucumber/service/CucumberTagProviderStrategy.java
+++ src/main/java/net/serenitybdd/cucumber/service/CucumberTagProviderStrategy.java
@@ -33,4 +33,9 @@ public class CucumberTagProviderStrategy implements TagProviderStrategy {
         return Lists.newArrayList((TagProvider) new FileSystemRequirementsTagProvider(environmentVariables,rootDirectory));
     }
 
+    @Override
+    public boolean hasHighPriority() {
+        return false;
+    }
+
 }
