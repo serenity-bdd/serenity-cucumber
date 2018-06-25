52fa6bc86cfd8d45831ae1d27d79e8ccceefa3d5
cliviu <liviu.carausu@gmail.com>
2018-06-25 23:35:44 +0200

    upgrade to cucumber 3.0.2 - not needed anymore to add plugin to each feature file

--- src/test/java/net/serenitybdd/cucumber/integration/BrokenStepLibraryScenario.java
+++ src/test/java/net/serenitybdd/cucumber/integration/BrokenStepLibraryScenario.java
@@ -8,5 +8,5 @@ import org.junit.runner.RunWith;
  * Created by john on 23/07/2014.
  */
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(plugin="cucumber.runtime.formatter.SerenityReporter",features="src/test/resources/samples/scenario_with_a_broken_step_definition_class.feature")
+@CucumberOptions(features="src/test/resources/samples/scenario_with_a_broken_step_definition_class.feature")
 public class BrokenStepLibraryScenario {}
