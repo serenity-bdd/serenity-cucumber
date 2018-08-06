e2fc48c523733757da6ddf75523fc9740b3f86ec
John Ferguson Smart <john.smart@wakaleo.com>
2018-08-06 18:51:53 +0100

    Release preparation

--- src/main/java/cucumber/runtime/formatter/SerenityReporter.java
+++ src/main/java/cucumber/runtime/formatter/SerenityReporter.java
@@ -347,7 +347,7 @@ public class SerenityReporter implements Formatter {
         String requestedDriverOptions = getDriverOptionsFrom(tags);
         if (isNotEmpty(requestedDriver)) {
             ThucydidesWebDriverSupport.useDefaultDriver(requestedDriver);
-            ThucydidesWebDriverSupport.useDriverOptions(requestedDriverOptions);
+            //TODO: ThucydidesWebDriverSupport.useDriverOptions(requestedDriverOptions);
         }
     }
 
